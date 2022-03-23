59a8a8bd6038c64da59a189867c3e201ae815c7a
Dalton <slattery.da@gmail.com>
2022-03-23 09:28:56 -0500

    commit tree 820279a4091933ae30a39fab66b199881e5ee792

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,29 @@
+class RecipesController < ApplicationController
+    def index
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: Recipe.all, include: [:user], status: :ok
+        else
+            render json: {errors:["Not authorized"]}, status: :unauthorized
+        end
+    end
+
+    def create
+        user = User.find_by(id: session[:user_id])
+        if user
+            recipe = user.recipes.create(recipe_params)
+            if recipe.valid?
+                render json: recipe, include: [:user], status: :created
+            else
+                render json: {errors: ["Unprocessable Entity"]}, status: :unprocessable_entity
+            end
+        else
+            render json: {errors: ["Not authorized"]}, status: :unauthorized
+        end
+    end
+
+    private
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+end
